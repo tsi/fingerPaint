b5e267c97525bb5000047c2d2481d60a05009c8d
tsi <tsi.shlidor@gmail.com>
2018-03-28 15:24:04 +0300

    Build and dev-server

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var watch = require('gulp-watch');
+
+// Path Variables
+global.paths = {
+  srcDir: 'src',
+  destDir: 'www'
+};
+
+// Start the server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: paths.srcDir
+    },
+    files: paths.srcDir + "/**/*"
+  });
+});
+
+gulp.task('default', ['browser-sync']);
